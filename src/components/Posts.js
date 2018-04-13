471950b1be3c7737b330fadfcd28722c3b8c166a
jjade seravla <jadealvares@hotmail.co.uk>
2018-04-13 12:08:43 +0100

    fetching the post and putting them into the render

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentWillMount(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      //fetch returns a promise with a result that needs to map to json
+      .then(data => this.setState({ posts: data }));
+    //here is where you make a fetch request, you also could use axios
+  }
+
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
     return (
       <div>
         <h1>Posts</h1>
+        { postItems }
       </div>
     )
   }
