e2ecc894a888a2ace8df45980a2d765cd0baf155
jjade seravla <jadealvares@hotmail.co.uk>
2018-04-13 14:49:36 +0100

    can now write in form on view

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -10,6 +10,7 @@ class PostForm extends Component {
     //each input value (title and body) will be part of our component's state
 
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
@@ -19,11 +20,31 @@ class PostForm extends Component {
     //allow you to now type inside form on view
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+    //becasue this is a post request, need additional parameter of an object
+  }
+
   render() {
     return (
       <div>
         <h1>Add Post</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>Title: </label><br />
             <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
