83efa1690680c1e3e5619f5e857002d54d446c63
jjade seravla <jadealvares@hotmail.co.uk>
2018-04-25 11:06:43 +0100

    posts coming in from redux state

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'; //the provider we brought in from react redux library as well as connect, it connects components from redux store that was provided by the provideer component
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
   // constructor(props){
@@ -8,16 +10,17 @@ class Posts extends Component {
   //   }
   // } commented out cos dont need components state anymore cos post is going to come from redux (application state)
 
-  //componentWillMount(){
+  componentWillMount(){
+    this.props.fetchPosts(); //calls fatch action
     // fetch('https://jsonplaceholder.typicode.com/posts')
     //   .then(res => res.json())
     //   //fetch returns a promise with a result that needs to map to json
     //   .then(data => this.setState({ posts: data }));
     // //here is where you make a fetch request, you also could use axios
-  //}
+  }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -32,4 +35,8 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts); //map our state to our properties. fetchPosts is a function.  Posts is the components.  fetchPosts will just call fetch
